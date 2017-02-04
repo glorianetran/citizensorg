bd3d426b2d8649e6165e49cef835ba615564dbb6
Denise <ddekker@mills.edu>
2017-02-04 22:32:03 +0000

    Denise first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var ctrlMain = require('../controllers/main');
+
+/* GET home page.*/
+router.get('/', ctrlMain.index);
+
+/* GET home page. 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});*/
+
+module.exports = router;
