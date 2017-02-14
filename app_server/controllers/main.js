5169596cedc32bc3769b63437c54bc6e9357a98a
Gloriane <glorianetran@gmail.com>
2017-02-14 22:23:30 +0000

    new routes added

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,24 @@
 /* GET home page.*/
-module.exports.index = function (req, res){
-    res.render('index',{title: 'CitizensOrg'});
-};
+// module.exports.index = function (req, res){
+//     res.render('index',{title: 'CitizensOrg'});
+// };
 
 module.exports.homepage = function (req, res){
     res.render('homepage',{title: 'CitizensOrg'});
+};
+
+module.exports.about = function (req, res){
+    res.render('about',{title: 'About'});
+};
+
+module.exports.register = function (req, res){
+    res.render('register',{title: 'Register'});
+};
+
+module.exports.login = function (req, res){
+    res.render('login',{title: 'Login'});
+};
+
+module.exports.findyourcongressperson = function (req, res){
+    res.render('findCongress',{title: 'findyourcongressperson'});
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,7 +5,20 @@ var ctrlMain = require('../controllers/main');
 
 /* GET home page.*/
 // router.get('/', ctrlMain.index);
-router.get('/', ctrlMain.homepage)
+router.get('/',ctrlMain.homepage);
+
+/* GET about page. */ 
+router.get('/about', ctrlMain.about);
+
+/* GET login page. */ 
+router.get('/login', ctrlMain.login);
+
+/* GET register page. */ 
+router.get('/register', ctrlMain.register);
+
+/* GET findyourcongressperson page. */ 
+router.get('/findyourcongressperson', ctrlMain.findyourcongressperson);
+
 
 /* GET home page. 
 router.get('/', function(req, res, next) {
