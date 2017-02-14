08b825f6caa916ea76ff72b047b98fa1751ffad6
Gloriane <glorianetran@gmail.com>
2017-02-14 18:04:53 +0000

    background image updated

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -2,3 +2,7 @@
 module.exports.index = function (req, res){
     res.render('index',{title: 'CitizensOrg'});
 };
+
+module.exports.homepage = function (req, res){
+    res.render('homepage',{title: 'CitizensOrg'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
 /* GET home page.*/
-router.get('/', ctrlMain.index);
+// router.get('/', ctrlMain.index);
+router.get('/', ctrlMain.homepage)
 
 /* GET home page. 
 router.get('/', function(req, res, next) {
