87a86641b80e3e750b3c5254d9112dd462d1e2a6
wangjames <wang.james.y@gmail.com>
2017-02-14 06:54:37 +0000

    additional changes

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+<<<<<<< HEAD
 
 var index = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+=======
+var passport = require('passport'); 
+require('./app_api/models/db');
+require('./app_api/config/passport'); 
+
+var routes = require('./app_server/routes/index');
+// var users = require('./app_server/routes/users');
+var routesApi = require('./app_api/routes/index');
+
+>>>>>>> e5996dd... additional changes
 
 var app = express();
 
@@ -21,9 +33,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+<<<<<<< HEAD
 
 app.use('/', index);
 app.use('/users', users);
+=======
+app.use(passport.initialize());
+app.use('/', routes);
+app.use('/api', routesApi);
+// app.use('/users', users);
+>>>>>>> e5996dd... additional changes
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
