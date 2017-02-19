175384785d00ebf09fbee584fadff84ec71bd67e
wangjames <wang.james.y@gmail.com>
2017-02-19 21:24:39 +0000

    finished user auth

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 var ctrlMain = require('../controllers/main');
 
 /* GET home page.*/
@@ -10,11 +11,6 @@ router.get('/',ctrlMain.homepage);
 /* GET about page. */ 
 router.get('/about', ctrlMain.about);
 
-/* GET login page. */ 
-router.get('/login', ctrlMain.login);
-
-/* GET register page. */ 
-router.get('/register', ctrlMain.register);
 
 /* GET findyourcongressperson page. */ 
 router.get('/findyourcongressperson', ctrlMain.findyourcongressperson);
@@ -27,5 +23,16 @@ router.get('/dashboard', ctrlMain.dashboard);
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });*/
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location/:locationid', ctrlLocations.locationInfo);
+router.get('/location/:locationid/review/new', ctrlLocations.addReview);
+router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
+router.get('/login', ctrlLocations.login);
+router.get('/register', ctrlLocations.register);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+
 
 module.exports = router;
