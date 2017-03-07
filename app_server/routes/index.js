ac948dfb86dadb88032de12e4ce71c4acc8a27d0
Raymond Thai <raymondthai8@gmail.com>
2017-03-07 00:08:10 +0000

    james and raymond test

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,7 +5,7 @@ var ctrlMain = require('../controllers/main');
 
 /* GET home page.*/
 // router.get('/', ctrlMain.index);
-router.get('/',ctrlMain.homepage);
+router.get('/', ctrlMain.homepage);
 
 /* GET about page. */ 
 router.get('/about', ctrlMain.about);
@@ -17,8 +17,10 @@ router.get('/login', ctrlMain.login);
 router.get('/register', ctrlMain.register);
 
 /* GET findyourcongressperson page. */ 
-router.get('/findyourcongressperson', ctrlMain.findyourcongressperson);
-
+/*router.get('/findyourcongressperson', ctrlMain.findyourcongressperson);
+router.post('/findyourcongressperson', ctrlMain.findpost);
+router.get('/findcongressresults/:zipcode', ctrlMain.results);
+*/
 /* GET dashboard page. */ 
 router.get('/dashboard', ctrlMain.dashboard);
 
