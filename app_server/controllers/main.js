02b877800eda10958c79431dc096ecdc1fac645e
Raymond Thai <raymondthai8@gmail.com>
2017-03-07 00:15:08 +0000

    raymond and james testing part 2

---
+++ app_server/controllers/main.js
@@ -0,0 +1,47 @@
+/* GET home page.*/
+// module.exports.index = function (req, res){
+//     res.render('index',{title: 'CitizensOrg'});
+// };
+
+module.exports.homepage = function (req, res){
+    res.render('homepage',{title: 'CitizensOrg'});
+};
+
+module.exports.about = function (req, res){
+    res.render('about',{title: 'About'});
+};
+
+module.exports.register = function (req, res){
+    res.render('register',{title: 'Register'});
+};
+
+module.exports.login = function (req, res){
+    res.render('login',{title: 'Login'});
+};
+
+module.exports.findyourcongressperson = function (req, res){
+    res.render('findCongress',{title: 'findyourcongressperson'});
+};
+
+module.exports.results = function (req, res) {
+	var apiurl = "https://congress.api.sunlightfoundation.com/legislators/locate?zip=" + req.params.zipcode;
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', apiurl, false);
+	xhr.send();
+	var text = xhr.responseText
+	var textObject = JSON.parse(text);
+
+	res.render('findCongressResults', textObject);
+	
+    }
+
+module.exports.findpost = function(req, res)
+{	
+	var zipcode = req.body.zipcode;
+	var url = "/findCongressResults/" + zipcode
+	res.redirect(url);
+}
+
+module.exports.dashboard = function (req, res){
+    res.render('dashboard',{title: 'dashboard'});
+};
